49bfd593d2f39953ef225256d2e1484dcb8bbc37
nhmayssi <n.hmayssi@gmail.com>
2026-02-13 11:23:24 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '/master_cv_vault/', 
   build: {
     outDir: 'dist',
     emptyOutDir: true,
